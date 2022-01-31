53c9c85b081f38024b5e6d481b7c3716a96a302e
Andrea Cantagallo <andrea_cantagallo@libero.it>
2022-01-31 19:40:25 +0100

    Align repo

--- app/src/main/java/it/mwt/myhealth/MainActivity.java
+++ app/src/main/java/it/mwt/myhealth/MainActivity.java
@@ -1,5 +1,6 @@
 package it.mwt.myhealth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
@@ -17,36 +19,50 @@ import it.mwt.myhealth.model.ClinicLocation;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextInputEditText loginUsername;
-    private TextInputEditText loginPassword;
-    private Button btnSignIn;
+    BottomNavigationView bottomNavigationView;
+
+    public static final String KEY_USERNAME = "username";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_menu);
 
-        System.out.println("onCreate");
+        bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
 
-        loginUsername = findViewById(R.id.loginUsername);
-        loginPassword = findViewById(R.id.loginPassword);
-        btnSignIn = findViewById(R.id.btnSignIn);
+    }
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = loginUsername.getText().toString();
-                if (username.isEmpty()) {
-                    loginUsername.setError(getString(R.string.field_empty));
-                    return;
-                }
-
-                String password = loginPassword.getText().toString();
-                if (password.isEmpty()) {
-                    loginPassword.setError(getString(R.string.field_empty));
-                    return;
-                }
-
+//    private TextInputEditText loginUsername;
+//    private TextInputEditText loginPassword;
+//    private Button btnSignIn;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        System.out.println("onCreate");
+//
+//        loginUsername = findViewById(R.id.loginUsername);
+//        loginPassword = findViewById(R.id.loginPassword);
+//        btnSignIn = findViewById(R.id.btnSignIn);
+//
+//        btnSignIn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String username = loginUsername.getText().toString();
+//                if (username.isEmpty()) {
+//                    loginUsername.setError(getString(R.string.field_empty));
+//                    return;
+//                }
+//
+//                String password = loginPassword.getText().toString();
+//                if (password.isEmpty()) {
+//                    loginPassword.setError(getString(R.string.field_empty));
+//                    return;
+//                }
+//
 //                List<ClinicLocation> listC = new ArrayList<>();
 //                ClinicLocation c = new ClinicLocation();
 //                c.setName("Test");
@@ -64,42 +80,42 @@ public class MainActivity extends AppCompatActivity {
 //                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
 //                intent.putExtra(MenuActivity.KEY_USERNAME, username);
 //                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        System.out.println("onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        System.out.println("onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        System.out.println("onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        System.out.println("onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        System.out.println("onDestroy");
-    }
+//            }
+//        });
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        System.out.println("onStart");
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        System.out.println("onResume");
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        System.out.println("onPause");
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//
+//        System.out.println("onStop");
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//
+//        System.out.println("onDestroy");
+//    }
 }
