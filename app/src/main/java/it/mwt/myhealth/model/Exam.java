d8452a2da3a0fbfdf561a01b2d1917c2dd508233
umbeboss987 <www.umbe_96@yahoo.it>
2022-02-07 19:29:29 +0100

    Model exam

---
+++ app/src/main/java/it/mwt/myhealth/model/Exam.java
@@ -0,0 +1,70 @@
+package it.mwt.myhealth.model;
+
+import java.io.Serializable;
+
+public class Exam implements Serializable {
+
+    private long id;
+    private String code;
+    private String name;
+    private Double price;
+    private String specialization;
+    private String subSpecialization;
+    private String type;
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getSubSpecialization() {
+        return subSpecialization;
+    }
+
+    public void setSubSpecialization(String subSpecialization) {
+        this.subSpecialization = subSpecialization;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+}
