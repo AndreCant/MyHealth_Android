b672e36eb43a5bf2a8e3e6083ae2b07f6427b949
Andrea Cantagallo <andrea_cantagallo@libero.it>
2022-02-18 20:08:05 +0100

    Updates

--- app/src/main/java/it/mwt/myhealth/ui/myExam/MyExamsActivity.java
+++ app/src/main/java/it/mwt/myhealth/ui/myExam/MyExamsActivity.java
@@ -1,12 +1,16 @@
 package it.mwt.myhealth.ui.myExam;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import it.mwt.myhealth.R;
+import it.mwt.myhealth.ui.category.CategoriesFragment;
 import it.mwt.myhealth.ui.exam.ExamViewModel;
+import it.mwt.myhealth.ui.login.LoginActivity;
+import it.mwt.myhealth.util.Preferences;
 
 public class MyExamsActivity extends AppCompatActivity {
 
@@ -18,8 +22,13 @@ public class MyExamsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_exams);
         getSupportActionBar().hide();
 
-        viewModel = new ViewModelProvider(this).get(MyExamsViewModel.class);
-        //viewModel.setType(getIntent().getSerializableExtra("type").toString());
-        viewModel.retrieveData(getApplicationContext());
+        if (!Preferences.isLogged(getApplicationContext())) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }else {
+            viewModel = new ViewModelProvider(this).get(MyExamsViewModel.class);
+            viewModel.retrieveData(getApplicationContext());
+        }
     }
 }
